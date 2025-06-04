be52f85c1b005a6fa8fcf8a28892de8c3c2a5447
1iverea9er <82063500+1iverea9er@users.noreply.github.com>
2025-06-05 00:58:42 +0300

    Update __init__.py

--- custom_components/comfort_tool/__init__.py
+++ custom_components/comfort_tool/__init__.py
@@ -1,7 +1,16 @@
+from homeassistant.config_entries import ConfigEntry
+from homeassistant.core import HomeAssistant
+
 DOMAIN = "comfort_tool"
 
-async def async_setup(hass, config):
+async def async_setup(hass: HomeAssistant, config: dict):
     return True
 
-async def async_setup_entry(hass, entry):
+async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry):
+    hass.async_create_task(
+        hass.config_entries.async_forward_entry_setup(entry, "sensor")
+    )
     return True
+
+async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry):
+    return await hass.config_entries.async_forward_entry_unload(entry, "sensor")
