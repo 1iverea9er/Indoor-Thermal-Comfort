0f04caedfbeb707c75547bebbdc74981d847f7db
1iverea9er <82063500+1iverea9er@users.noreply.github.com>
2025-06-05 10:14:39 +0300

    Update sensor.py

--- custom_components/comfort_tool/sensor.py
+++ custom_components/comfort_tool/sensor.py
@@ -1,68 +1,62 @@
-from homeassistant.components.sensor import SensorEntity
-from homeassistant.helpers.event import async_track_state_change
-from homeassistant.core import callback
 
-from .const import DOMAIN
-from .lib.comfort_tool.comfort import pmv_ppd
+import logging
+from homeassistant.components.sensor import SensorEntity
+from homeassistant.const import TEMP_CELSIUS
+from .comfort import calculate_thermal_comfort
+
+_LOGGER = logging.getLogger(__name__)
 
 async def async_setup_entry(hass, config_entry, async_add_entities):
+    _LOGGER.debug("Setting up comfort_tool sensor entry")
     options = config_entry.options
-    if not all(k in options for k in ("ta", "tr", "va", "rh", "clo", "met")):
+
+    required = ("ta", "tr", "va", "rh", "clo", "met")
+    if not all(k in options for k in required):
+        _LOGGER.error("Missing one or more required options: %s", required)
         return
 
-    sensor = ComfortSensor(hass, config_entry)
+    sensor = PMVSensor(hass, options)
     async_add_entities([sensor])
+    _LOGGER.debug("Sensor entity added: %s", sensor.name)
 
-class ComfortSensor(SensorEntity):
-    def __init__(self, hass, config_entry):
+class PMVSensor(SensorEntity):
+    def __init__(self, hass, options):
+        _LOGGER.debug("Initializing PMVSensor with options: %s", options)
         self._hass = hass
-        self._config_entry = config_entry
+        self._options = options
+        self._attr_name = "Thermal Comfort PMV"
+        self._attr_unit_of_measurement = TEMP_CELSIUS
+        self._attr_unique_id = "thermal_comfort_pmv"
         self._state = None
-        self._attr_name = "PMV"
-        self._attr_unique_id = f"{config_entry.entry_id}_pmv"
 
-        self.entity_ids = [
-            config_entry.options["ta"],
-            config_entry.options["tr"],
-            config_entry.options["va"],
-            config_entry.options["rh"],
-            config_entry.options["clo"],
-            config_entry.options["met"],
-        ]
+        self._ta = options["ta"]
+        self._tr = options["tr"]
+        self._va = options["va"]
+        self._rh = options["rh"]
+        self._clo = options["clo"]
+        self._met = options["met"]
 
-        async_track_state_change(hass, self.entity_ids, self._state_changed)
+        for entity_id in (self._ta, self._tr, self._va, self._rh, self._clo, self._met):
+            hass.helpers.event.async_track_state_change_event(entity_id, self._state_changed)
 
     @property
-    def state(self):
+    def native_value(self):
         return self._state
 
-    @property
-    def unit_of_measurement(self):
-        return "PMV"
-
-    @callback
-    def _state_changed(self, entity_id, old_state, new_state):
+    async def _state_changed(self, event):
+        _LOGGER.debug("State changed detected for event: %s", event)
         try:
             states = self._hass.states
-            ta = float(states.get(self._config_entry.options["ta"]).state)
-            tr = float(states.get(self._config_entry.options["tr"]).state)
-            va = float(states.get(self._config_entry.options["va"]).state)
-            rh = float(states.get(self._config_entry.options["rh"]).state)
-            clo = float(states.get(self._config_entry.options["clo"]).state)
-            met = float(states.get(self._config_entry.options["met"]).state)
+            ta = float(states.get(self._ta).state)
+            tr = float(states.get(self._tr).state)
+            va = float(states.get(self._va).state)
+            rh = float(states.get(self._rh).state)
+            clo = float(states.get(self._clo).state)
+            met = float(states.get(self._met).state)
 
-            pmv, _ = pmv_ppd(
-                ta=ta,
-                tr=tr,
-                vel=va,
-                rh=rh,
-                clo=clo,
-                met=met,
-                wme=0,
-                standard="ashrae"
-            )
-
-            self._state = round(pmv, 2)
+            results = calculate_thermal_comfort(ta, tr, va, rh, clo, met)
+            self._state = round(results["pmv"], 2)
+            _LOGGER.debug("Calculated PMV: %s", self._state)
             self.async_write_ha_state()
-        except Exception:
-            self._state = None
+        except Exception as e:
+            _LOGGER.error("Error computing thermal comfort: %s", e)
