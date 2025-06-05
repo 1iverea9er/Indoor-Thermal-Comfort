878ce611f8da436e8dce853916cf154c0ac27a88
1iverea9er <82063500+1iverea9er@users.noreply.github.com>
2025-06-05 10:36:11 +0300

    Update __init__.py

--- custom_components/comfort_tool/__init__.py
+++ custom_components/comfort_tool/__init__.py
@@ -1,16 +1,21 @@
+
+import logging
 from homeassistant.config_entries import ConfigEntry
 from homeassistant.core import HomeAssistant
 
+_LOGGER = logging.getLogger(__name__)
+
 DOMAIN = "comfort_tool"
 
-async def async_setup(hass: HomeAssistant, config: dict):
-    return True
+async def async_setup_entry(hass: HomeAssistant, config_entry: ConfigEntry) -> bool:
+    hass.data.setdefault(DOMAIN, {})
+    hass.data[DOMAIN][config_entry.entry_id] = config_entry.data
 
-
-async def async_setup_entry(hass, config_entry):
     await hass.config_entries.async_forward_entry_setups(config_entry, ["sensor"])
     return True
 
-async def async_unload_entry(hass, config_entry):
-    return await hass.config_entries.async_unload_platforms(config_entry, ["sensor"])
-
+async def async_unload_entry(hass: HomeAssistant, config_entry: ConfigEntry) -> bool:
+    unload_ok = await hass.config_entries.async_unload_platforms(config_entry, ["sensor"])
+    if unload_ok:
+        hass.data[DOMAIN].pop(config_entry.entry_id)
+    return unload_ok
