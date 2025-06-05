f993408e6bc64e4dee9bb9eafec2d0667aaee16a
1iverea9er <82063500+1iverea9er@users.noreply.github.com>
2025-06-05 10:49:14 +0300

    Update __init__.py

--- custom_components/comfort_tool/__init__.py
+++ custom_components/comfort_tool/__init__.py
@@ -4,23 +4,17 @@ from homeassistant.config_entries import ConfigEntry
 from homeassistant.core import HomeAssistant
 
 _LOGGER = logging.getLogger(__name__)
+
 DOMAIN = "comfort_tool"
+PLATFORMS = ["sensor"]
 
 async def async_setup_entry(hass: HomeAssistant, config_entry: ConfigEntry) -> bool:
     hass.data.setdefault(DOMAIN, {})
     hass.data[DOMAIN][config_entry.entry_id] = config_entry.data
-
-    async def _forward():
-        try:
-            await hass.config_entries.async_forward_entry_setups(config_entry, ["sensor"])
-        except Exception as e:
-            _LOGGER.exception("Failed to forward setup to sensor platform: %s", e)
-
-    hass.async_create_task(_forward())
     return True
 
 async def async_unload_entry(hass: HomeAssistant, config_entry: ConfigEntry) -> bool:
-    unload_ok = await hass.config_entries.async_unload_platforms(config_entry, ["sensor"])
+    unload_ok = await hass.config_entries.async_unload_platforms(config_entry, PLATFORMS)
     if unload_ok:
         hass.data[DOMAIN].pop(config_entry.entry_id)
     return unload_ok
