6f354b189260c8d99efeba6a4c99b8afb2a2e71d
1iverea9er <82063500+1iverea9er@users.noreply.github.com>
2025-06-05 23:11:43 +0300

    Update __init__.py

--- custom_components/comfort_tool/__init__.py
+++ custom_components/comfort_tool/__init__.py
@@ -1,18 +1,34 @@
 from homeassistant.config_entries import ConfigEntry
 from homeassistant.core import HomeAssistant
+from homeassistant.helpers.update_coordinator import DataUpdateCoordinator, UpdateFailed
+from datetime import timedelta
+import logging
 
 from .const import DOMAIN
 
+_LOGGER = logging.getLogger(__name__)
+
 PLATFORMS = ["sensor"]
 
-
 async def async_setup_entry(hass: HomeAssistant, config_entry: ConfigEntry) -> bool:
-    # Регистрируем конфигурацию
-    hass.data.setdefault(DOMAIN, {})[config_entry.entry_id] = {}
+    async def async_update_data():
+        return {}
+
+    coordinator = DataUpdateCoordinator(
+        hass,
+        _LOGGER,
+        name="comfort_tool",
+        update_method=async_update_data,
+        update_interval=timedelta(minutes=5),
+    )
+    await coordinator.async_config_entry_first_refresh()
+
+    hass.data.setdefault(DOMAIN, {})[config_entry.entry_id] = {
+        "coordinator": coordinator,
+    }
 
     await hass.config_entries.async_forward_entry_setups(config_entry, PLATFORMS)
     return True
-
 
 async def async_unload_entry(hass: HomeAssistant, config_entry: ConfigEntry) -> bool:
     unload_ok = await hass.config_entries.async_unload_platforms(config_entry, PLATFORMS)
