c835424edff33a85c6c11ec499e2762a816a5ac3
1iverea9er <82063500+1iverea9er@users.noreply.github.com>
2025-06-11 22:59:36 +0300

    Update __init__.py

--- custom_components/comfort_tool/__init__.py
+++ custom_components/comfort_tool/__init__.py
@@ -1,17 +1,23 @@
+import logging
 from homeassistant.config_entries import ConfigEntry
 from homeassistant.core import HomeAssistant
 from homeassistant.helpers.typing import ConfigType
 
 from .const import DOMAIN
 
+_LOGGER = logging.getLogger(__name__)
+
 async def async_setup(hass: HomeAssistant, config: ConfigType) -> bool:
+    _LOGGER.debug("Setting up Comfort Tool domain")
     return True
 
 async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
+    _LOGGER.debug("Setting up entry: %s", entry.entry_id)
     hass.async_create_task(
         hass.config_entries.async_forward_entry_setup(entry, "sensor")
     )
     return True
 
 async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
+    _LOGGER.debug("Unloading entry: %s", entry.entry_id)
     return await hass.config_entries.async_forward_entry_unload(entry, "sensor")
