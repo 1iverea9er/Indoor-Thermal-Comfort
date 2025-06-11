9537609c58ac62dc561274570825c30bbe28081c
1iverea9er <82063500+1iverea9er@users.noreply.github.com>
2025-06-11 22:54:33 +0300

    Update __init__.py

--- custom_components/comfort_tool/__init__.py
+++ custom_components/comfort_tool/__init__.py
@@ -1,37 +1,17 @@
 from homeassistant.config_entries import ConfigEntry
 from homeassistant.core import HomeAssistant
-from homeassistant.helpers.update_coordinator import DataUpdateCoordinator, UpdateFailed
-from datetime import timedelta
-import logging
+from homeassistant.helpers.typing import ConfigType
 
 from .const import DOMAIN
 
-_LOGGER = logging.getLogger(__name__)
-
-PLATFORMS = ["sensor"]
-
-async def async_setup_entry(hass: HomeAssistant, config_entry: ConfigEntry) -> bool:
-    async def async_update_data():
-        return {}
-
-    coordinator = DataUpdateCoordinator(
-        hass,
-        _LOGGER,
-        name="comfort_tool",
-        update_method=async_update_data,
-        update_interval=timedelta(minutes=5),
-    )
-    await coordinator.async_config_entry_first_refresh()
-
-    hass.data.setdefault(DOMAIN, {})[config_entry.entry_id] = {
-        "coordinator": coordinator,
-    }
-
-    await hass.config_entries.async_forward_entry_setups(config_entry, PLATFORMS)
+async def async_setup(hass: HomeAssistant, config: ConfigType) -> bool:
     return True
 
-async def async_unload_entry(hass: HomeAssistant, config_entry: ConfigEntry) -> bool:
-    unload_ok = await hass.config_entries.async_unload_platforms(config_entry, PLATFORMS)
-    if unload_ok:
-        hass.data[DOMAIN].pop(config_entry.entry_id)
-    return unload_ok
+async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
+    hass.async_create_task(
+        hass.config_entries.async_forward_entry_setup(entry, "sensor")
+    )
+    return True
+
+async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
+    return await hass.config_entries.async_forward_entry_unload(entry, "sensor")
