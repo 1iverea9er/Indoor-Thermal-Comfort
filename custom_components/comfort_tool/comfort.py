1e64d1e9679dacc4730ba28bd217059ffbb7655b
1iverea9er <82063500+1iverea9er@users.noreply.github.com>
2025-06-11 22:51:41 +0300

    Update comfort.py

--- custom_components/comfort_tool/comfort.py
+++ custom_components/comfort_tool/comfort.py
@@ -1,8 +1,40 @@
+from pythermalcomfort.models import pmv_ppd, set_tmp, cooling_effect
+
 def calculate_thermal_comfort(ta, tr, va, rh, clo, met):
-    """
-    Простейшая модель комфорта. Можно заменить реальным расчётом PMV/PPD или другим.
-    """
+    res = {}
     try:
-        return round((ta + tr + va + rh + clo + met) / 6, 2)
+        ppd_data = pmv_ppd(tdb=ta, tr=tr, vr=va, rh=rh, clo=clo, met=met)
+        res["pmv"] = round(ppd_data.get("pmv"), 2)
+        res["ppd"] = round(ppd_data.get("ppd"), 1)
+
+        set_data = set_tmp(tdb=ta, tr=tr, vr=va, rh=rh, clo=clo, met=met)
+        res["set"] = round(set_data.get("set"), 2)
+
+        ce_data = cooling_effect(tdb=ta, tr=tr, vr=va, rh=rh, clo=clo, met=met, v_relative=0.3)
+        res["ce"] = round(ce_data.get("ce"), 2)
+
+        pmv = res.get("pmv")
+        if pmv is not None:
+            if pmv < -2.5:
+                ts = "Very Cold"
+            elif pmv < -1.5:
+                ts = "Cold"
+            elif pmv < -0.5:
+                ts = "Cool"
+            elif pmv < 0.5:
+                ts = "Neutral"
+            elif pmv < 1.5:
+                ts = "Warm"
+            elif pmv < 2.5:
+                ts = "Hot"
+            else:
+                ts = "Very Hot"
+        else:
+            ts = None
+
+        res["ts"] = ts
+
     except Exception:
-        return None
+        res = {k: None for k in ["pmv", "ppd", "set", "ce", "ts"]}
+
+    return res
