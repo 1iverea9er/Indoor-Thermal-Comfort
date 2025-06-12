5b019cecd85c48b5c251df59c2f6ed3f6f390d52
1iverea9er <82063500+1iverea9er@users.noreply.github.com>
2025-06-12 19:40:24 +0300

    Update util.py

--- custom_components/comfort_tool/util.py
+++ custom_components/comfort_tool/util.py
@@ -80,3 +80,13 @@ def CtoF(x):
 
 def FtoC(x):
     return (x - 32) * 5 / 9
+
+def find_saturated_vapor_pressure_torr(T):
+    """
+    Calculates saturated vapor pressure (in Torr) at temperature T (°C)
+    Based on equation: exp(18.6686 - 4030.183 / (T + 235.0))
+
+    :param T: Temperature in degrees Celsius
+    :return: Saturated vapor pressure in Torr
+    """
+    return math.exp(18.6686 - 4030.183 / (T + 235.0))
