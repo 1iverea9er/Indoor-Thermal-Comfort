0ca3cf0f5318062682d37584b85b26e5365b3635
1iverea9er <82063500+1iverea9er@users.noreply.github.com>
2025-06-14 21:32:31 +0300

    Update config_flow.py

--- custom_components/comfort_tool/config_flow.py
+++ custom_components/comfort_tool/config_flow.py
@@ -24,7 +24,7 @@ class ComfortToolConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
 
     async def async_step_user(self, user_input=None):
         if user_input is not None:
-            return self.async_create_entry(title="Comfort Tool", data=user_input)
+            return self.async_create_entry(title="Indoor Thermal Comfort", data=user_input)
 
         return self.async_show_form(step_id="user", data_schema=CONFIG_SCHEMA)
 
